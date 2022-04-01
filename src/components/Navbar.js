70b16903ce8e07b06d66ba748f4569eeec3968c9
Jalamang <Jalamangjanka@gmail.com>
2022-04-01 10:10:57 -0400

    Bootstrap for styles, React router v6 - app navigation, Redux toolkit - state management

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const isAuth = false;
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        
+        <div>
+          <NavLink to="/">
+            <span className="navbar-brand mb-0 h1">Home</span>
+          </NavLink>
+        </div>
+        {isAuth ? (
+          <div>
+            <NavLink to="/dashboard" className="mx-3">
+              <span>Dashboard</span>
+            </NavLink>
+          </div>
+        ) : (
+          <div>
+            <NavLink to="/login">
+              <span>Login</span>
+            </NavLink>
+            <NavLink to="/register" className="mx-3">
+              <span>Register</span>
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
